059045a93958f2b1b76e2ef11f67c164683a740f
Charlotte <charlotte.waegeneire@hotmail.fr>
2023-07-04 16:36:08 +0200

    add table component

--- src/components/toolsContainer.jsx
+++ src/components/toolsContainer.jsx
@@ -1,34 +1,56 @@
-"use client";
-import { useEffect, useState } from "react";
+"use client"
+import { useEffect, useState } from "react"
+import Table from "@/components/table"
 
-const ToolsContainer = ({ title, script }) => {
-  const [scriptIsRunning, setScriptIsRunning] = useState(false);
-  const [stringIsRunning, setStringIsRunning] = useState("");
+const ToolsContainer = ({ title }) => {
+  const [scriptIsRunning, setScriptIsRunning] = useState(false)
+  const [stringIsRunning, setStringIsRunning] = useState("")
 
-  const handleScriptIsRunning = () => setScriptIsRunning(!scriptIsRunning);
+  const columns = ["Port", "State", "Service", "Product"]
+  const data = [{
+    "Port": "53",
+    "Protocol": "tcp",
+    "State": "open",
+    "Reason": "syn-ack",
+    "Reason TTL": "127",
+    "Service": "domain",
+    "Product": "Simple DNS Plus",
+    "OSType": "Windows"
+  }, {
+    "Port": "88",
+    "Protocol": "tcp",
+    "State": "open",
+    "Reason": "syn-ack",
+    "Reason TTL": "127",
+    "Service": "kerberos-sec",
+    "Product": "Microsoft Windows Kerberos",
+    "OSType": "Windows"
+  }]
+
+  const handleScriptIsRunning = () => setScriptIsRunning(!scriptIsRunning)
 
   useEffect(() => {
     if (scriptIsRunning) {
-      let string = "";
+      let string = ""
       const updateString = () => {
         if (string.length === 3) {
-          string = "";
+          string = ""
         } else {
-          string += ".";
+          string += "."
         }
-        setStringIsRunning(string);
-      };
+        setStringIsRunning(string)
+      }
 
-      const interval = setInterval(updateString, 1000);
+      const interval = setInterval(updateString, 1000)
 
       return () => {
-        clearInterval(interval);
-      };
+        clearInterval(interval)
+      }
     } else {
-      setStringIsRunning("");
+      setStringIsRunning("")
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [scriptIsRunning]);
+  }, [scriptIsRunning])
 
   return (
     <div className="flex gap-5 flex-col w-full bg-base-100 rounded-lg p-4">
@@ -39,31 +61,35 @@ const ToolsContainer = ({ title, script }) => {
         <pre data-prefix=">">
           <code>Exécution du script : </code>
         </pre>
-        {scriptIsRunning ? (
+        {scriptIsRunning && (
           <pre data-prefix=">" className="text-warning">
-            <code>{`En cours d'éxécution ${stringIsRunning}`}</code>
+            <code>{`En cours d'exécution ${stringIsRunning}`}</code>
           </pre>
-        ) : null}
-        {/* {scriptIsRunning ? (
+        )}
+        {/* {scriptIsFinished && (
           <pre data-prefix=">" className="text-success">
             <code>Fini!</code>
           </pre>
-        ) : null} */}
+        )} */}
       </div>
-      <button
-        className="btn bg-[#45781e] w-42 self-end text-white"
-        onClick={handleScriptIsRunning}
-      >
-        Lancer le test
-      </button>
-      <button
-        className="btn bg-[#45781e] w-42 self-end text-white"
-        onClick={handleScriptIsRunning}
-      >
-        Stopper le test
-      </button>
+      <div className="flex gap-2 justify-end">
+        <button
+          className="btn bg-[#45781e] w-42 self-end text-white"
+          onClick={handleScriptIsRunning}
+          disabled={scriptIsRunning}
+        >
+          Lancer le test
+        </button>
+      </div>
+      {data && (
+        <Table
+          columns={columns}
+          data={data}
+        />
+        )}
     </div>
-  );
-};
+  )
+}
 
-export default ToolsContainer;
+
+export default ToolsContainer
