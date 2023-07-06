a7cebaad06f9d902bb7b1f034d502da85bc7e60a
Charlotte <charlotte.waegeneire@hotmail.fr>
2023-07-06 17:38:12 +0200

    ez

--- src/components/menuDrawer.jsx
+++ src/components/menuDrawer.jsx
@@ -42,6 +42,7 @@ const MenuDrawer = () => {
               alt="laser saber"
               width={80}
               height={45}
+              className={"object-scale-down"}
             />
             <div>{"Reconnaissance"}</div>
           </div>
@@ -66,6 +67,7 @@ const MenuDrawer = () => {
               alt="laser saber"
               width={80}
               height={45}
+              className={"object-scale-down"}
             />
             <div>{"Simulation Ransomware"}</div>
           </div>
