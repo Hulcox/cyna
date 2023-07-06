18654f07e06754dbc98945fbc542a70ff3575e7b
Charlotte <charlotte.waegeneire@hotmail.fr>
2023-07-06 17:28:08 +0200

    missclick

--- src/components/noData.jsx
+++ src/components/noData.jsx
@@ -2,10 +2,10 @@ const NoData = () => {
   return (
     <div className="flex flex-col gap-5">
       <h1 className="font-bold uppercase text-xl">
-        Aucune donnée reccueillie
+        Aucune donnée recueillie
       </h1>
       <div>
-        Lancez une attaque pour commencer à reccueillir des données
+        Lancez une attaque pour commencer à recueillir des données
       </div>
     </div>
   )
