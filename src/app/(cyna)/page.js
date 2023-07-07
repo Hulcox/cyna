46fe413b65d602e4b270e8bdc87483bf5a8919f5
Charlotte <charlotte.waegeneire@hotmail.fr>
2023-07-07 13:56:13 +0200

    Added dynamic cyberscore ez

--- src/app/(cyna)/page.js
+++ src/app/(cyna)/page.js
@@ -5,6 +5,7 @@ import NoData from "@/components/noData"
 import CyberScore from "@/components/cyberScore"
 
 export default function Home() {
+  const [cyberScore, setCyberScore] = useState("E")
   const [data, setData] = useState([])
 
   const files = [
@@ -27,15 +28,42 @@ export default function Home() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
+  useEffect(() => {
+    const file = "openvas"
+    fetch(`/docs/${file}.json`)
+      .then((response) => response.json())
+      .then((data) => {
+        const cvssValues = data.map((entry) => entry.CVSS)
+
+        let score = 0
+        let w = 0
+        for (const cvss of cvssValues) {
+          score += cvss * (Math.floor(cvss) ** 2)
+          w += Math.floor(cvss) ** 2
+        }
+
+        let cyberScore = score / w
+        cyberScore = cyberScore / 2
+
+        const tmp = ["A", "B", "C", "D", "E"]
+        const result = tmp[Math.floor(cyberScore)]
+        setCyberScore(result)
+      })
+      .catch((error) => {
+        console.log(`Une erreur s'est produite lors du chargement du fichier JSON : ${error.message}`)
+      })
+  }, [])
+
+
   return (
     <div className="p-10 w-3/4">
       {data && data.map((info, index) => (
-        <div className="flex flex-col gap-10" key={index} >
+        <div className="flex flex-col gap-10" key={index}>
           <div className="flex flex-col gap-5">
             <h1 className="font-bold uppercase text-xl">
               Nos données actuelles
             </h1>
-            <CyberScore cyberScore={"D"} />
+            <CyberScore cyberScore={cyberScore}/>
           </div>
           <BarChart info={info}/>
         </div>
