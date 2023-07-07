b93a1b0865aeda936677069e15fc24cca12f29fb
Hulcox <hulcox03@gmail.com>
2023-07-07 11:50:39 +0200

    oui monsieur

--- src/components/toolsContainer.jsx
+++ src/components/toolsContainer.jsx
@@ -1,23 +1,23 @@
-"use client"
-import { useCallback, useContext, useEffect, useState } from "react"
-import Table from "@/components/table"
-import AppContext from "./appContext"
-import { Formik, Form, Field } from "formik"
+"use client";
+import { useCallback, useContext, useEffect, useState } from "react";
+import Table from "@/components/table";
+import AppContext from "./appContext";
+import { Formik, Form, Field } from "formik";
 
 const ToolsContainer = ({ title, scriptName }) => {
-  const [data, setData] = useState([])
-  const [stringIsRunning, setStringIsRunning] = useState("")
+  const [data, setData] = useState([]);
+  const [stringIsRunning, setStringIsRunning] = useState("");
   const { scriptIsBusy, handleSetScriptIsBusy, handleSetScriptIsDone } =
-    useContext(AppContext)
-  const [isAnyRunning, setIsAnyRunning] = useState(false)
+    useContext(AppContext);
+  const [isAnyRunning, setIsAnyRunning] = useState(false);
 
   useEffect(() => {
     const isAnyRunning =
       Object.values(scriptIsBusy).filter(
         (script) => script.status === "running"
-      ).length > 0
-    setIsAnyRunning(isAnyRunning)
-  }, [scriptIsBusy])
+      ).length > 0;
+    setIsAnyRunning(isAnyRunning);
+  }, [scriptIsBusy]);
 
   const columns = [
     "IP",
@@ -25,8 +25,8 @@ const ToolsContainer = ({ title, scriptName }) => {
     "CVSS",
     "Severity",
     "Cyberscore",
-    "Specific Result"
-  ]
+    "Specific Result",
+  ];
 
   // const ToolSchema = Yup.object().shape({
   //   command: Yup.string().required("Une commande est requis !"),
@@ -35,61 +35,60 @@ const ToolsContainer = ({ title, scriptName }) => {
 
   const handleStartScript = useCallback(
     async (value) => {
-      console.log(value)
-      handleSetScriptIsBusy(scriptName)
+      console.log(value);
+      handleSetScriptIsBusy(scriptName);
 
-      // const run = await request.post("/run", {
-      //   container: scriptName,
-      //   command: value.command,
-      //   params: value.params,
-      // });
+      const run = await request.post("/run", {
+        container: scriptName,
+        command: value.command,
+        params: value.params,
+      });
 
       setTimeout(() => {
-        handleSetScriptIsDone(scriptName)
-      }, 5000)
-
+        handleSetScriptIsDone(scriptName);
+      }, 10000);
     },
     // eslint-disable-next-line react-hooks/exhaustive-deps
     []
-  )
+  );
 
   useEffect(() => {
     if (scriptIsBusy[scriptName].status === "running") {
-      let string = ""
+      let string = "";
       const updateString = () => {
         if (string.length === 3) {
-          string = ""
+          string = "";
         } else {
-          string += "."
+          string += ".";
         }
-        setStringIsRunning(string)
-      }
+        setStringIsRunning(string);
+      };
 
-      const interval = setInterval(updateString, 1000)
+      const interval = setInterval(updateString, 1000);
 
       return () => {
-        clearInterval(interval)
-      }
+        clearInterval(interval);
+      };
     } else {
-      setStringIsRunning("")
+      setStringIsRunning("");
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [scriptIsBusy])
+  }, [scriptIsBusy]);
 
   useEffect(() => {
     fetch(`/docs/${scriptName}.json`)
       .then((response) => response.json())
       .then((info) => {
-        setData(info)
+        setData(info);
       })
       .catch((error) => {
         console.error(
           "Une erreur s'est produite lors du chargement du fichier JSON :",
           error
-        )
-      })
+        );
+      });
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+  }, []);
 
   return (
     <div className="flex gap-5 flex-col w-full bg-base-100 rounded-lg p-4">
@@ -116,7 +115,7 @@ const ToolsContainer = ({ title, scriptName }) => {
         <Formik
           initialValues={{
             command: "",
-            params: ""
+            params: "",
           }}
           onSubmit={handleStartScript}
         >
@@ -145,9 +144,11 @@ const ToolsContainer = ({ title, scriptName }) => {
           )}
         </Formik>
       </div>
-      {scriptIsBusy[scriptName].status === "done" && <Table columns={columns} data={data}/>}
+      {scriptIsBusy[scriptName].status === "done" && (
+        <Table columns={columns} data={data} />
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default ToolsContainer
+export default ToolsContainer;
